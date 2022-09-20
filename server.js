34074eb89b7a0633d301058c2c537c543e2dd1fe
James Anh <james.anh184@gmail.com>
2022-09-20 12:38:23 -0700

    able to create user

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const db = require("./config/connection");
+const {User, Reaction, Thought} = require('./models')
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
-// app.use(routes)
+
+
+// GET /api/users
+app.post('/api/users',({body}, res) => {
+    try {
+        User.create(body).then(Userdb => res.json(Userdb))
+    } catch (error) {
+        res.status(404).json(err)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 db.once('open', () => {
-    app.listen(port, () => console.log(`Connected to ${port}`));
-})
+    app.listen(port, () => console.log(`Connected to localhost at  port ${port}!!`));
+  } )
+
+
+
+
+
+
+
+
+
+
