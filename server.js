e5b02c098534d7c4ba3cae482773f4ee29e392b0
James Anh <james.anh184@gmail.com>
2022-09-20 17:10:08 -0700

    initialize thoughts control

--- server.js
+++ server.js
@@ -102,9 +102,81 @@ app.post('/api/users/:userId/friends/:friendId', ({params}, res) => {
         res.status(404).json(error)
     }
 })
+// This is THOUGHT section
+// Create thoughts 
+// POST /api/thoughts
+app.post('/api/thoughts', ({body}, res) => {
+    try {
+        Thought.create(body).then(Thoughtdb => {
+            User.findOneAndUpdate({_id: body.userId}, {$push: {thoughts: Thoughtdb._id}}, {new: true}).then(Userdb => {
+                if(!Userdb) {
+                    res.status(404).json({message: 'No user found'});
+                    return;
+                }
+                res.json(Userdb)
+            }).catch(err => res.json(err))
 
+        })
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
-
+// get all thoughts
+// GET /api/thoughts
+app.get('/api/thoughts', (req, res) => {
+    Thought.findOne({}).populate({path: 'reactions', select:'-__v' }).select('-__v').then(Thoughtdb => res.json(Thoughtdb)).
+    catch(err => res.status(500).json(err))
+})
+// get thought by ID
+// GET /api/thoughts/:id
+app.get('/api/thoughts/:id', ({params}, res) => {
+    try {
+        Thought.findOne({_id: params.id}).populate({path: 'reactions', select:'-__v' }).select('-__v').
+        then(Thoughtdb => {
+            if(!Thoughtdb) {
+                res.status(404).json({message: "User not found"});
+                return;
+            }
+            res.json(Thoughtdb)
+        } )
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+// Update thought by id
+// PUT /api/thoughts/:id
+app.put('/api/thoughts/:id', ({params, body}, res) => {
+    try {
+        Thought.findOneAndUpdate({ _id: params.id}, body, {new: true}).
+        then(Thoughtdb => {
+            if(!Thoughtdb) {
+                res.status(404).json({message: 'No Thought found'});
+                return;
+            }
+            res.json(Thoughtdb)
+        } )
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+// Deelete thought
+// DELETE /api/thoughts/:id
+app.delete('/api/thoughts/:id', ({params}, res) => {
+    try {
+        Thought.findOneAndDelete({_id: params.id}).then(Thoughtdb =>{
+            if(!Thoughtdb) {
+                res.status(404).json({message: "No thought found with this user"});
+                return;
+            }
+            User.findOneAndUpdate({username: Thoughtdb.username}, {$pull: {thoughts: params.id}}).then(() => {
+                res.json({message: "Successfully delete thought"})
+            }).catch(err => res.status(500).json(err));
+        })
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
 
 
 
